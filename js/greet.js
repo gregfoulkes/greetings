d674f1c26ce991ccca7e40835afe708b5aeb3fcd
gregfoulkes <greg_foulkes@live.com>
2018-04-11 12:49:32 +0200

    add all changes

--- js/greet.js
+++ js/greet.js
@@ -9,38 +9,85 @@ var greetBtn = document.querySelector('.greetMeBtn')
 var displayCount = document.querySelector('.counterDisplay')
 
 function btnRadioCheck(){
-  var checkedRadioBtn = document.querySelector("input[name='languageRadio']:checked");
+  var checkedRadioBtn = document.querySelector("input[name='languageType']:checked");
     if (checkedRadioBtn){
       var greetingType = checkedRadioBtn.value
     }
   return greetingType
 }
 
-function greetMe(){
-  var person = nameValue.value
-
-  if(person != ''){
-    var thePerson = ""
-    thePerson = person
-  }
-  return thePerson
+function greetNameValue(){
+var person = nameValue.value
+return person
 }
 var greetNumber = 0
 
-function greetCounter(){
-  greetNumber++;
-  return greetNumber;
+function greetFunction(){
+
+  function greetMe(person){
+
+    if(person != ''){
+      var thePerson = ""
+      thePerson = person
+
+    }
+    return thePerson
+  }
+
+
+
+  function greetCounter(){
+      greetNumber = greetNumber+1;
+    return greetNumber;
+    }
+  //var greetType = ''
+  function languageSelect(type){
+
+
+      if(type === "english"){
+        type = 'Hello'
+      }
+
+      if(type === "afrikaans"){
+        type = 'More'
+      }
+
+      if(type === 'xhosa'){
+        type = 'Molo'
+      }
+      return type
+    }
+
+  return{
+    language: languageSelect,
+    greet: greetMe,
+    greetCount: greetCounter,
+  }
 }
 
-function displayName(thePerson){
-  var grt = greetCounter()
-  displayCount.innerHTML = grt
-  nameDisplay.innerHTML = thePerson
+
+function displayName(){
+  var greetVariable = greetFunction()
+  var gn = greetVariable.greet(greetNameValue())
+  var gt = greetVariable.language(btnRadioCheck())
+  nameDisplay.innerHTML = gt + ' ' + gn
+
+
+}
+
+function displayCounter(){
+   var greetVariable = greetFunction()
+  displayCount.innerHTML = greetVariable.greetCount()
+
 }
 
 
 
  greetBtn.addEventListener('click', function(){
-   displayName(greetMe())
+   var greetVariable = greetFunction()
 
- })
+  // greetFunction.greetCounter()
+   displayName()
+   displayCounter()
+   //greetVariable.greetCount()
+ });
